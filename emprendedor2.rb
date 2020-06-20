9047bb4d6af6ea0913c4b8dfa9271343d6b9133d
KiraElric <lissette.urso@gmail.com>
2020-06-20 03:45:43 -0400

    desafio emprendedor2

--- emprendedor2.rb
+++ emprendedor2.rb
@@ -1,8 +1,10 @@
 precio = ARGV[0].to_i
 usuarios = ARGV[1].to_i
-gastos = ARGV[2].to_i
+usuarios_premium = ARGV[2].to_i
+usuarios_gratuitos = ARGV[3].to_i
+gastos = ARGV[4].to_i
 
-utilidades = ((precio*usuarios) - gastos)
+utilidades = (precio*(usuarios + 2*usuarios_premium) - gastos)
 
 if utilidades > 0
     impuesto = (utilidades*0.35).round(0)
@@ -11,4 +13,3 @@ if utilidades > 0
 else
     puts "Su utilidad es #{utilidades}, se encuentra excento de impuesto"
 end
-
